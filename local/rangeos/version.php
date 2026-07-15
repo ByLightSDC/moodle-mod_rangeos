061a71a96031b144298d0624ad359cde7f029942
Megan Bohland <megan.bohland@bylight.com>
2026-07-15 15:43:04 -0400

    chore: release 0.4.0

--- local/rangeos/version.php
+++ local/rangeos/version.php
@@ -17,10 +17,10 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'local_rangeos';
-$plugin->version = 2026042319;
+$plugin->version = 2026071514;
 $plugin->requires = 2024100700; // Moodle 4.5+.
 $plugin->maturity = MATURITY_ALPHA;
-$plugin->release = '0.3.0';
+$plugin->release = '0.4.0';
 $plugin->dependencies = [
     'mod_cmi5' => 2026022601,
 ];
